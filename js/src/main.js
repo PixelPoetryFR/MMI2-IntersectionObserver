54dd316841903d691a8c594eb3f959118c982fe2
PixelPoetryFR <julien@pixelpoetry.fr>
2021-10-07 17:25:35 +0200

    ADD structure

---
+++ js/src/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  'use strict';
+  'esversion: 6';
+
+
+
+
+
+
+
+
+
+
+});// document.addEventListener('DOMContentLoaded', function() {
